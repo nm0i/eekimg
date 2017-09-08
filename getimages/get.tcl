9897fe4a420a6658babeebbdda16e541f8571718
nm0i <nm0i@me0w.net>
2017-09-08 21:04:06 +0300

    fetcher works more or less

--- getimages/get.tcl
+++ getimages/get.tcl
@@ -5,36 +5,51 @@ term::ansi::send::import vt
 vt::init
 
 package require sha256
+package require Tclx
 
 package require sqlite3
 sqlite db urlmap.db
 
-catch {                      
-    db eval {                
-        CREATE TABLE IF NOT EXISTS urlmap(                  
-										 hash TEXT,                 
-										 url TEXT,               
-										 t INTEGER,                 
-										 CONSTRAINT urlmap_pk PRIMARY KEY(hash))                                                 
-    }                        
+catch {
+	db eval {
+		CREATE TABLE IF NOT EXISTS urlmap(
+										hash TEXT,
+										url TEXT,
+										t INTEGER,
+										CONSTRAINT urlmap_pk PRIMARY KEY(hash))
+	}
 }
 
 set fileID [open urls.txt r]
 
 while {[gets $fileID line] != -1} {
 	set key [::sha2::sha256 $line]
-    catch {
-		vt::sda_fgblue
-		exec -ignorestderr snarf -m $line "images/${key}"
+	catch {
+
+		set response [db eval {
+			SELECT * FROM urlmap WHERE hash = :key
+		}]
+
+		if {$response != ""} {
+			vt::sda_fgred
+			vt::wr "URL:$line\nAlready exists in the db, skipping.\n"
+			continue
+		}
 
 		set now [clock seconds]
 		
 		vt::sda_fgyellow
-		vt::wr "$now $key | $line\n"    
-		db eval {    
+		vt::wr "$now $key | $line\n"
+		db eval {
 			INSERT OR REPLACE INTO urlmap (hash,url,t) VALUES (:key,:line,:now);                         
-		}            		
+		}
+		
+		vt::sda_fgblue
+		exec -ignorestderr wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36" --no-check-certificate $line -O "images/${key}"
+		sleep 2
 	}
 }
+
+db close
 vt::sda_reset
 
